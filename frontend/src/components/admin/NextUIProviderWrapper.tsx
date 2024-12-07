54e01c18eea19daf2e61945675bed9ca51a3b499
forest6511 <20209757+forest6511@users.noreply.github.com>
2024-12-08 08:41:06 +0900

    feat v4: implement create category

---
+++ frontend/src/components/admin/NextUIProviderWrapper.tsx
@@ -0,0 +1,6 @@
+'use client'
+import {NextUIProvider} from "@nextui-org/react";
+
+export default function NextUIProviderWrapper({ children }: { children: React.ReactNode }) {
+    return <NextUIProvider>{children}</NextUIProvider>
+}

ad84193bed439d52464a357e5ce62e8b4add4bb8
forest6511 <20209757+forest6511@users.noreply.github.com>
2025-01-04 07:51:58 +0900

    optimizeCss.

--- frontend-user/next.config.ts
+++ frontend-user/next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizeCss: true,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        punycode: false,
+      };
+    }
+    return config;
+  },
 }
 
 export default nextConfig

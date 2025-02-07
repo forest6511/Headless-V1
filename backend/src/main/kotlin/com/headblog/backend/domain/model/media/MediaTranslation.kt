ca8047f6105ec7778069bf62b614f70d6d8ea280
forest6511 <20209757+forest6511@users.noreply.github.com>
2025-02-08 05:10:47 +0900

    MediaTranslation

--- backend/src/main/kotlin/com/headblog/backend/domain/model/media/MediaTranslation.kt
+++ backend/src/main/kotlin/com/headblog/backend/domain/model/media/MediaTranslation.kt
@@ -2,7 +2,7 @@ package com.headblog.backend.domain.model.media
 
 import com.headblog.backend.domain.model.common.Language
 
-data class Translation(
+data class MediaTranslation(
     val language: Language,
     val title: String,
 )
